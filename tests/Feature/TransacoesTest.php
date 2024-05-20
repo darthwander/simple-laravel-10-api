68e46efce3b422631e8b023692e23c5df0ea305f
Wander Araujo <wander_a@hotmail.com>
2024-05-20 14:02:48 -0300

    Criando novos helpers para os testes de transacoes para melhor organização e legibilidade do código.

--- tests/Feature/TransacoesTest.php
+++ tests/Feature/TransacoesTest.php
@@ -22,24 +22,16 @@ describe('Caso Positivo', function () {
     
     it('Deve executar uma transação', function ($forma_pagamento, $taxa)
     {
-        $data = [ 
-            "forma_pagamento" => $forma_pagamento,
-            "numero_conta" =>  $numero_conta = $this->numeroContaTest,
-            "valor" => $valor = 94.10
-        ];
+        $data = buildData(forma_pagamento: $forma_pagamento);
+
         $response = $this->postJson('/api/v1/transacao', $data);
     
-        $saldo_esperado = calcularSaldoEsperado($valor, $taxa);
+        defaultPositiveAssertions(
+            response: $response,
+            expectedStatusCode: 201, 
+            taxa: $taxa
+        );      
        
-        $response->assertStatus(201);
-        $response->assertJsonStructure([
-                'saldo',
-                'numero_conta'
-            ]);
-        
-        $res = $response->getData(true);
-        expect($res['numero_conta'])->toEqual($numero_conta);
-        expect($res['saldo'])->toEqual($saldo_esperado);
     })->with('formasPagamentoComTaxa');
 });
 
@@ -47,65 +39,57 @@ describe('Casos Negativos', function () {
     
     it('Não deve executar a transação, pois não tem saldo suficiente', function ($forma_pagamento)
     {
-        $data = [ 
-            "forma_pagamento" => $forma_pagamento,
-            "numero_conta" => $this->numeroContaTest,
-            "valor" => $valor = 100.1
-        ];
+        $data = buildData(forma_pagamento: $forma_pagamento, valor: 100.1);
+
         $response = $this->postJson('/api/v1/transacao', $data);
             
-        $res = $response->getData(true);
-        expect($res)->toHaveKey('message');
-        expect($res['message'])->toEqual("Saldo insuficiente");
-        $response->assertStatus(404);
-    })->with('formasPagamento');;
+        defaultNegativeAssertions(
+            response: $response,
+            expectedStatusCode: 404,
+            message: "Saldo insuficiente"
+        );
+
+    })->with('formasPagamento');
     
     it('Não deve executar a transação, pois o valor é inválido', function ($forma_pagamento, $valor)
     {
-        $data = [ 
-            "forma_pagamento" => $forma_pagamento,
-            "numero_conta" => $this->numeroContaTest,
-            "valor" => $valor
-        ];
+        $data = buildData(forma_pagamento: $forma_pagamento, valor: $valor);
+
         $response = $this->postJson('/api/v1/transacao', $data);
             
-        $res = $response->getData(true);
-        expect($res)->toHaveKey('message');
-        expect($res['message'])->toEqual("Validation error");
-        $response->assertStatus(422);
-        
+        defaultNegativeAssertions(
+            response: $response,
+            expectedStatusCode: 422,
+            message: "Validation error"
+        );        
     })
     ->with('formasPagamento')
     ->with('valoresInvalidos');
     
     it('Não deve executar a transação, pois a forma de pagamento é inválida', function ($forma_pagamento)
     {
-        $data = [ 
-            "forma_pagamento" => $forma_pagamento,
-            "numero_conta" => $this->numeroContaTest,
-            "valor" => 94.10
-        ];
+        $data = buildData(forma_pagamento: $forma_pagamento);
+
         $response = $this->postJson('/api/v1/transacao', $data);
             
-        $res = $response->getData(true);
-        expect($res)->toHaveKey('message');
-        expect($res['message'])->toEqual("Validation error");
-        $response->assertStatus(422);
+        defaultNegativeAssertions(
+            response: $response,
+            expectedStatusCode: 422,
+            message: "Validation error"
+        );
     })->with('formasPagamentoInvalidos');
 
     it('Não deve executar a transação, pois a conta não existe ou é inválida.', function ($forma_pagamento, $taxa, $numero_conta)
     {
-        $data = [ 
-            "forma_pagamento" => $forma_pagamento,
-            "numero_conta" => $numero_conta,
-            "valor" => 94.10
-        ];
+        $data = buildData(forma_pagamento: $forma_pagamento, numero_conta: $numero_conta);
+
         $response = $this->postJson('/api/v1/transacao', $data);
-            
-        $res = $response->getData(true);
-        expect($res)->toHaveKey('message');
-        expect($res['message'])->toEqual("Validation error");
-        $response->assertStatus(422);
+           
+        defaultNegativeAssertions(
+            response: $response,
+            expectedStatusCode: 422,
+            message: "Validation error"
+        );
     })
     ->with('formasPagamentoComTaxa')
     ->with('contasInvalidas');
--- tests/Helpers/TransacoesHelpers.php
+++ tests/Helpers/TransacoesHelpers.php
@@ -12,3 +12,35 @@ function calcularTransacao($valor, $taxa) : float
 {
     return round($valor + ($valor * $taxa), 2);
 }
+
+function defaultNegativeAssertions($response, $expectedStatusCode, $message) : void
+{    
+    $response->assertStatus($expectedStatusCode);
+    $response = $response->getData(true);
+    expect($response)->toHaveKey('message');
+    expect($response['message'])->toEqual($message);
+}
+
+function defaultPositiveAssertions($response, $expectedStatusCode, $taxa = 0, $numero_conta = 999999) : void
+{   
+    $response->assertStatus($expectedStatusCode);
+    $response->assertJsonStructure([
+        'saldo',
+        'numero_conta'
+    ]);
+
+    $saldo_esperado = calcularSaldoEsperado(valor: 94.10, taxa: $taxa);
+    $res = $response->getData(true);
+
+    expect($res['numero_conta'])->toEqual($numero_conta);
+    expect($res['saldo'])->toEqual($saldo_esperado);
+}
+
+function buildData($forma_pagamento, $valor = 94.10, $numero_conta = 999999) : array
+{
+    return [ 
+        "forma_pagamento" => $forma_pagamento,
+        "numero_conta" => $numero_conta,
+        "valor" => $valor
+    ];
+}
